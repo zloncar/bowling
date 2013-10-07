8158970e439d172dcce3c8f3efd2189df587ece4
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-07 12:03:34 +0100

    1st

---
+++ bowling_spec.rb
@@ -0,0 +1,37 @@
+describe "ten-pin bowling" do
+
+  PINS = 10
+
+  it "one try" do
+    try = Try.new
+    try.score = 8
+    expect( try.score).to eq( 8 )
+  end
+
+  it "one frame" do
+    try1 = Try.new
+    try1.score = 6
+    try2 = Try.new
+    try2.score = 3
+    frame = Frame.new
+    frame.score = [try1.score, try2.score]
+    expect( frame.score ).to eq( 9 )
+  end
+
+  class Try
+    attr_accessor :score
+  end
+
+  class Frame
+
+    def score
+      @score
+    end
+
+    def score=( scores )
+      @score = scores.first + scores.last
+    end
+
+  end
+
+end

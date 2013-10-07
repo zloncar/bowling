808bac5f2c3ef4ada78de112463a4dea444d4576
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-07 18:33:39 +0100

    Compare two total are the same.

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -1,6 +1,7 @@
 describe "ten-pin bowling" do
 
   MAX_PINS = 10
+  $total = 0
 
   it "one try" do
     try = Try.new( 8 )
@@ -17,8 +18,7 @@ describe "ten-pin bowling" do
 
   it "one line no spares no strikes" do
     game = play
-    puts "total = #{game.total}"
-#   expect( total ).to eq( 17 )
+    expect( game.total ).to eq( $total )
   end
 
   def play
@@ -29,12 +29,17 @@ describe "ten-pin bowling" do
     def initialize
       @line = Hash.new( 0 )
       (1..10).each do |frame|
-        @line[frame] = [ ( rand() * 10 ).to_i, ( rand() * 10 ).to_i ]
+        try1 = ( rand() * 10 ).to_i
+        try2 = ( rand() * 10 ).to_i
+        $total += try1
+        @line[frame] = [ try1, try2 ]
       end
     end
+
     def line
       @line
     end
+
     def total
       @total = 0
       @line.each { |k, v| @total += v[0] }
@@ -46,15 +51,18 @@ describe "ten-pin bowling" do
     def initialize( score )
       @score = score
     end
+
     def score
       @score
     end
   end
 
+
   class Frame
     def score
       @score
     end
+
     def score=( scores )
       @score = scores.first.score + scores.last.score
     end

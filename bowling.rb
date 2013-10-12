b0f1d09d7bae5c8c4645c9468d9dc03edbba3c8f
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-12 20:41:11 +0100

    Get rid of Line class and pass two simple strike tests.

--- bowling.rb
+++ bowling.rb
@@ -1,17 +1,13 @@
 MAX_PINS = 10
-class Line
-
-  def score( rolls, total, spare )
-    if rolls.size.zero?
-      total
-    else
-      frame_score = rolls[0,2].inject(:+)
-      total += ( frame_score * spare )
-      spare = rolls[0,2].inject(:+) == MAX_PINS ? 2 : 1
-      2.times { rolls.shift }
-      spare = 1 if rolls.size == 1
-      score( rolls, total, spare )
-    end
+def score( rolls, total, spare )
+  if rolls.size.zero?
+    total
+  else
+    frame_score = rolls[0,2].inject(:+)
+    total += ( frame_score * spare )
+    spare = rolls[0,2].inject(:+) == MAX_PINS ? 2 : 1
+    2.times { rolls.shift }
+    spare = 1 if rolls.size == 1
+    score( rolls, total, spare )
   end
-
 end
--- bowling_spec.rb
+++ bowling_spec.rb
@@ -2,28 +2,39 @@ require_relative 'bowling.rb'
 
 describe "ten-pin bowling" do
 
-  it "total from rolls - none cleared" do
-    line = Line.new
+  it "rolls - none cleared" do
     roll = [ 2, 3, 7, 1, 4, 4 ]
-    expect( line.score( roll, 0, 1 ) ).to eq( 21 )
+    expect( score( roll, 0, 1 ) ).to eq( 21 )
   end
 
-  it "total from rolls - one spare" do
-    line = Line.new
+  it "rolls - one spare" do
     roll = [ 2, 3, 7, 3, 4, 4 ]
-    expect( line.score( roll, 0, 1 ) ).to eq( 31 )
+    expect( score( roll, 0, 1 ) ).to eq( 31 )
   end
 
-  it "total from rolls - spares" do
-    line = Line.new
+  it "rolls - few spares" do
     roll = [ 2, 8, 7, 3, 4, 4, 9, 1, 2, 2 ]
-    expect( line.score( roll, 0, 1 ) ).to eq( 64 )
+    expect( score( roll, 0, 1 ) ).to eq( 64 )
   end
 
-  it "total from rolls - spare at the end" do
-    line = Line.new
-    roll = [ 2, 3, 4, 6, 4]
-    expect( line.score( roll, 0, 1 ) ).to eq( 19 )
+  it "rolls - spare at the end" do
+    roll = [ 2, 3, 4, 6, 4 ]
+    expect( score( roll, 0, 1 ) ).to eq( 19 )
+  end
+
+  it "rolls - one strike at the end" do
+    roll = [ 10, 0, 3 ]
+    expect( score( roll, 0, 1 ) ).to eq( 13 )
+  end
+
+  it "rolls - one strike at the end" do
+    roll = [ 10, 0, 3 ]
+    expect( score( roll, 0, 1 ) ).to eq( 13 )
+  end
+
+  it "rolls - strike - not at the end" do
+    roll = [ 10, 0, 3, 5 ]
+    expect( score( roll, 0, 1 ) ).to eq( 26 )
   end
 
 end

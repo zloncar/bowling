54fd2128bd19d4238a52ffa6c893cb9c4ac8e3c1
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-12 13:12:59 +0100

    Use recurrence to pass throws with spares.

--- bowling.rb
+++ bowling.rb
@@ -1,33 +1,15 @@
 MAX_PINS = 10
 class Line
 
-  def score( pins )
-    total = 0
-    if pins[ -4, 2 ].inject(:+) >= MAX_PINS
-      total = total + MAX_PINS + 2 * pins[-2, 2].inject(:+)
-      4.times { pins.pop }
-      total += pins.inject(:+)
+  def score( rolls, total, spare )
+    if rolls.size.zero?
+      total
     else
-      pins.inject(:+)
+      total += ( rolls[0,2].inject(:+) * spare )
+      spare = rolls[0,2].inject(:+) == 10 ? 2 : 1
+      2.times { rolls.shift }
+      score( rolls, total, spare )
     end
   end
 
 end
-
-
-def blah( a, total )
-  if a.size.zero?
-    total
-  else
-    total += a[0,2].inject(:+)
-    2.times { a.shift }
-    blah(a, total)
-  end
-end
-
-
-
-
-
-
-
--- bowling_spec.rb
+++ bowling_spec.rb
@@ -5,12 +5,12 @@ describe "ten-pin bowling" do
   it "total from rolls - none cleared" do
     line = Line.new
     roll = [ 2, 3, 7, 1, 4, 4 ]
-    expect( line.score( roll ) ).to eq( 21 )
+    expect( line.score( roll, 0, 1 ) ).to eq( 21 )
   end
 
   it "total from rolls - spares" do
     line = Line.new
     roll = [ 2, 3, 7, 3, 4, 4 ]
-    expect( line.score( roll ) ).to eq( 31 )
+    expect( line.score( roll, 0, 1 ) ).to eq( 31 )
   end
 end

7c241b9eac51aca2082068b296345939069358c7
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-12 13:33:57 +0100

    Spare is at the end

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -8,9 +8,22 @@ describe "ten-pin bowling" do
     expect( line.score( roll, 0, 1 ) ).to eq( 21 )
   end
 
-  it "total from rolls - spares" do
+  it "total from rolls - one spare" do
     line = Line.new
     roll = [ 2, 3, 7, 3, 4, 4 ]
     expect( line.score( roll, 0, 1 ) ).to eq( 31 )
   end
+
+  it "total from rolls - spares" do
+    line = Line.new
+    roll = [ 2, 8, 7, 3, 4, 4, 9, 1, 2, 2 ]
+    expect( line.score( roll, 0, 1 ) ).to eq( 64 )
+  end
+
+  it "total from rolls - spare at the end" do
+    line = Line.new
+    roll = [ 2, 3, 4, 6, 7 ]
+    expect( line.score( roll, 0, 1 ) ).to eq( 29 )
+  end
+
 end

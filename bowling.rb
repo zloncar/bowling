000bcd580c33fbac04048a44f9f12f81f50954e7
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-12 12:35:33 +0100

    Play with recurrence to calculate totals.

--- bowling.rb
+++ bowling.rb
@@ -12,6 +12,22 @@ class Line
     end
   end
 
-
 end
+
+
+def blah( a, total )
+  if a.size.zero?
+    total
+  else
+    total += a[0,2].inject(:+)
+    2.times { a.shift }
+    blah(a, total)
+  end
+end
+
+
+
+
+
+
 

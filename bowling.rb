619f1f5e811e4b4949b13b895f98bc9b35de763e
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-13 16:05:12 +0100

    Make spare into array to deal with strikes.

--- bowling.rb
+++ bowling.rb
@@ -4,10 +4,16 @@ def score( rolls, total, spare )
     total
   else
     frame_score = rolls[0,2].inject(:+)
-    total += ( frame_score * spare )
-    spare = rolls[0,2].inject(:+) == MAX_PINS ? 2 : 1
+    total += ( frame_score * spare.shift )
+    if rolls.first == MAX_PINS
+      2.times { spare.push( 2 ) }
+    elsif rolls[0,2].inject(:+) == MAX_PINS
+      spare << 2
+    else
+      spare << 1
+    end
     2.times { rolls.shift }
-    spare = 1 if rolls.size == 1
+    spare[0] = 1 if rolls.size == 1
     score( rolls, total, spare )
   end
 end
--- bowling_spec.rb
+++ bowling_spec.rb
@@ -4,42 +4,37 @@ describe "ten-pin bowling" do
 
   it "rolls - none cleared" do
     roll = [ 2, 3, 7, 1, 4, 4 ]
-    expect( score( roll, 0, 1 ) ).to eq( 21 )
+    expect( score( roll, 0, [1] ) ).to eq( 21 )
   end
 
   it "rolls - one spare" do
     roll = [ 2, 3, 7, 3, 4, 4 ]
-    expect( score( roll, 0, 1 ) ).to eq( 31 )
+    expect( score( roll, 0, [1] ) ).to eq( 31 )
   end
 
   it "rolls - few spares" do
     roll = [ 2, 8, 7, 3, 4, 4, 9, 1, 2, 2 ]
-    expect( score( roll, 0, 1 ) ).to eq( 64 )
+    expect( score( roll, 0, [1] ) ).to eq( 64 )
   end
 
   it "rolls - spare at the end" do
     roll = [ 2, 3, 4, 6, 4 ]
-    expect( score( roll, 0, 1 ) ).to eq( 19 )
+    expect( score( roll, 0, [1] ) ).to eq( 19 )
   end
 
   it "rolls - one strike at the end" do
     roll = [ 10, 0, 3 ]
-    expect( score( roll, 0, 1 ) ).to eq( 13 )
-  end
-
-  it "rolls - one strike at the end" do
-    roll = [ 10, 0, 3 ]
-    expect( score( roll, 0, 1 ) ).to eq( 13 )
+    expect( score( roll, 0, [1] ) ).to eq( 13 )
   end
 
   it "rolls - strike with only one frame left" do
     roll = [ 10, 0, 3, 5 ]
-    expect( score( roll, 0, 1 ) ).to eq( 26 )
+    expect( score( roll, 0, [1] ) ).to eq( 26 )
   end
 
   it "rolls - strike with two frames left" do
     roll = [ 10, 0, 3, 5, 2, 4 ]
-    expect( score( roll, 0, 1 ) ).to eq( 38 )
+    expect( score( roll, 0, [1] ) ).to eq( 38 )
   end
 
 end
